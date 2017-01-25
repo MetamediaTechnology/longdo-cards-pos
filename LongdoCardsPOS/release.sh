d8896d4021ec0c151b3f6a603fd21a8156a57519
TK <tanawat@mm.co.th>
2017-01-25 12:15:16 +0700

    releaase script

---
+++ LongdoCardsPOS/release.sh
@@ -0,0 +1,4 @@
+tar -czf LongdoCardsPOS.tgz -C bin/Release LongdoCardsPOS.exe QRCoder.dll WpfControls.dll
+scp -P 1222 LongdoCardsPOS.tgz tanawat@secure.longdo.com:/home/tanawat/deploy/LongdoCardsPOS.tgz
+rm LongdoCardsPOS.tgz
+
