8e4cbc92865c0a38fca329d64775558e8e52cbd3
TK <tanawat@mm.co.th>
2017-01-27 17:17:29 +0700

    build script

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-tar -czf LongdoCardsPOS.tgz -C LongdoCardsPOS/bin/Release LongdoCardsPOS.exe QRCoder.dll WpfControls.dll
-scp -P 1222 LongdoCardsPOS.tgz tanawat@secure.longdo.com:/home/tanawat/deploy/LongdoCardsPOS.tgz
-rm LongdoCardsPOS.tgz
-
+cd LongdoCardsPOS/bin/Release
+/c/Program\ Files/7-Zip/7z a -tzip LongdoCardsPOS.zip LongdoCardsPOS.exe QRCoder.dll WpfControls.dll
+scp -P 1222 LongdoCardsPOS.zip tanawat@secure.longdo.com:/home/tanawat/deploy/LongdoCardsPOS.zip
+rm LongdoCardsPOS.zip
